37cccc7715967065682737f4938a79e95880999c
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2019-05-06 11:33:14 +0430

    english vocabulary

---
+++ en.js
@@ -0,0 +1,59 @@
+const fs = require('fs-extra');
+const _ = require('lodash');
+const extract = require('extract-lemmatized-nonstop-words');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+(async () => {
+    const data = await fs.readFile('./data/en/frequency-alpha-gcide.txt', 'utf8');
+    let totalCount = 0;
+    const words = new Map();
+    data.split('\n').forEach(line => {
+        // [RANKING,WORD,COUNT,PERCENT,CUMULATIVE]
+        let [rank, word, count, percent, cumulative] = line.split(/[\s]+/);
+        const parsed = extract(word);
+        if (parsed.length != 1) return;
+        word = parsed[0].vocabulary;
+        try {
+            count = parseInt(count.replace(/,/g, ''));
+            totalCount += count;
+            const record = words.get(word);
+            if (record) {
+                words.set(word, record + count);
+            } else {
+                words.set(word, count);
+            }
+        } catch (error) {
+            debugger;
+        }
+    });
+    let list = Array.from(words).map(record => ({ word: record[0], count: record[1], percent: (record[1] * 100 / totalCount) }));
+    list = _.orderBy(list, 'count', 'desc');
+    let rank = 1;
+    list.reduce((cumulative, item) => {
+        cumulative += item.percent;
+        item.rank = rank++;
+        item.cumulative = cumulative.toFixed(6);
+        item.percent = item.percent.toFixed(6);
+        delete item.count;
+        return cumulative;
+    }, 0);
+    try {
+        // saving as JSON
+        await fs.writeJSON('./dist/en.json', list);
+        // saving as CSV
+        const csvWriter = createCsvWriter({
+            header: [
+                { id: 'rank' },
+                { id: 'word' },
+                { id: 'percent' },
+                { id: 'cumulative' },
+            ],
+            // header: ['RANKING', 'WORD', 'COUNT', 'PERCENT', 'CUMULATIVE'],
+            path: './dist/en.csv'
+        });
+        await csvWriter.writeRecords(list);
+    } catch (error) {
+        console.log(error);
+    }
+    console.log('...Done');
+})();
